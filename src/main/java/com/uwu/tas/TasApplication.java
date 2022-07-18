9c9b0551eee093c477fcc72ea484ff0526a1decd
Diluni Malsha Peiris <diluni.malsha.peiris@gmail.com>
2022-07-18 21:55:33 +0530

    Created Spring Boot project

---
+++ src/main/java/com/uwu/tas/TasApplication.java
@@ -0,0 +1,13 @@
+package com.uwu.tas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasApplication.class, args);
+	}
+
+}
