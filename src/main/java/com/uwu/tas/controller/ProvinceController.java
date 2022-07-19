23e51374fa945b760eb4ea8a0ed049d9e92ed72f
Sanidu-Udawatta <saniduhasanka@gmail.com>
2022-07-20 00:40:22 +0530

    Create Files

--- src/main/java/com/uwu/tas/controller/ProvinceController.java
+++ src/main/java/com/uwu/tas/controller/ProvinceController.java
@@ -1,19 +1,33 @@
 package com.uwu.tas.controller;
 
 
+import com.uwu.tas.dto.CommonResponse;
 import com.uwu.tas.dto.ProvinceDto;
+import com.uwu.tas.entity.Province;
+import com.uwu.tas.service.impl.ProvinceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/admin/province")
 public class ProvinceController {
 
+    private final ProvinceService provinceService;
+
     @PostMapping(value = "/save")
+    public ResponseEntity<CommonResponse<String>> saveProvince(@RequestBody ProvinceDto provinceDto) {
 
+        provinceService.saveProvince(provinceDto);
+        return ResponseEntity.ok(new CommonResponse<String>(true, "Province saved successfully!"));
+    }
 
+    @GetMapping(value = "/all")
+    public ResponseEntity<CommonResponse<List<ProvinceDto>>> getAllProvince(){
+        List<ProvinceDto> allProvince = provinceService.getAllProvince();
+        return ResponseEntity.ok(new CommonResponse<List<ProvinceDto>>(true,allProvince));
+    }
 }
---
+++ src/main/java/com/uwu/tas/repository/ProvinceRepository.java
@@ -0,0 +1,7 @@
+package com.uwu.tas.repository;
+
+import com.uwu.tas.entity.Province;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProvinceRepository extends JpaRepository<Province, Long> {
+}
