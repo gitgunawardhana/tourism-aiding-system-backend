e8bca45b333fcc90ada6b11c015908a30620bcf8
Diluni Malsha Peiris <diluni.malsha.peiris@gmail.com>
2022-07-23 01:28:39 +0530

    Created repositories

---
+++ src/main/java/com/uwu/tas/repository/VehicleRepository.java
@@ -0,0 +1,9 @@
+package com.uwu.tas.repository;
+
+import com.uwu.tas.entity.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+}
