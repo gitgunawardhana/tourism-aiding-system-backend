bc56c4db5cd34ec841480409d863bc628cae381a
Diluni Malsha Peiris <diluni.malsha.peiris@gmail.com>
2022-07-24 04:51:20 +0530

    Updated admin controllers

---
+++ src/main/java/com/uwu/tas/controller/admin/AdminActivityController.java
@@ -0,0 +1,67 @@
+package com.uwu.tas.controller.admin;
+
+import com.uwu.tas.dto.activity.ActivityDto;
+import com.uwu.tas.dto.CommonResponse;
+import com.uwu.tas.enums.VisibilityStatus;
+import com.uwu.tas.exception.CustomServiceException;
+import com.uwu.tas.repository.ActivityRepository;
+import com.uwu.tas.service.ActivityService;
+import com.uwu.tas.util.Base64Handler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/admin/activity")
+public class AdminActivityController {
+
+    private final ActivityService activityService;
+
+    private final ActivityRepository activityRepository;
+    private final Base64Handler base64Handler;
+
+    @PostMapping(value = "")
+    public ResponseEntity saveActivity(@RequestBody ActivityDto activityDto) {
+        try {
+            activityService.saveActivity(activityDto);
+            return ResponseEntity.ok(new CommonResponse<>(true, "Activity saved successfully!"));
+        } catch (CustomServiceException e) {
+            return ResponseEntity.ok(new CommonResponse<>(false, e.getMessage()));
+        }
+    }
+
+    @PutMapping(value = "")
+    public ResponseEntity updateActivity(@RequestBody ActivityDto activityDto) {
+        try {
+            activityService.updateActivity(activityDto);
+            return ResponseEntity.ok(new CommonResponse<>(true, "Activity updated successfully!"));
+        } catch (CustomServiceException e) {
+            return ResponseEntity.ok(new CommonResponse<>(false, e.getMessage()));
+        }
+    }
+
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity changeActivityStatus(@PathVariable(value = "id")long id, @RequestParam(value = "status")VisibilityStatus status) {
+        try {
+            activityService.changeActivityStatus(id, status);
+            return ResponseEntity.ok(new CommonResponse<>(true, "Activity status changed successfully!"));
+        } catch (CustomServiceException e) {
+            return ResponseEntity.ok(new CommonResponse<>(false, e.getMessage()));
+        }
+    }
+
+    @GetMapping(value = "")
+    public ResponseEntity getAllActivities() {
+        try {
+            List<ActivityDto> allActivities = activityService.getAllActivities();
+            return ResponseEntity.ok(new CommonResponse<>(true, allActivities));
+        } catch (CustomServiceException e) {
+            return ResponseEntity.ok(new CommonResponse<>(false, e.getMessage()));
+        }
+    }
+}
--- src/main/java/com/uwu/tas/controller/admin/AdminProvinceController.java
+++ src/main/java/com/uwu/tas/controller/admin/AdminProvinceController.java
@@ -1,7 +1,7 @@
 package com.uwu.tas.controller.admin;
 
 import com.uwu.tas.dto.CommonResponse;
-import com.uwu.tas.dto.ProvinceDto;
+import com.uwu.tas.dto.province.ProvinceDto;
 import com.uwu.tas.exception.CustomServiceException;
 import com.uwu.tas.service.ProvinceService;
 import lombok.RequiredArgsConstructor;
@@ -13,22 +13,28 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/admin/province")
-public class ProvinceController {
+public class AdminProvinceController {
 
     private final ProvinceService provinceService;
 
-    @PostMapping(value = "/save")
-    public ResponseEntity<CommonResponse<String>> saveProvince(@RequestBody ProvinceDto provinceDto) {
+    @PostMapping(value = "")
+    public ResponseEntity saveProvince(@RequestBody ProvinceDto provinceDto) {
         try {
             provinceService.saveProvince(provinceDto);
         } catch (CustomServiceException e) {
-            return ResponseEntity.ok(new CommonResponse<String>(false, e.getMessage()));
+            return ResponseEntity.ok(new CommonResponse<>(false, e.getMessage()));
         }
-        return ResponseEntity.ok(new CommonResponse<String>(true, "Province saved successfully!"));
+        return ResponseEntity.ok(new CommonResponse<>(true, "Province saved successfully!"));
     }
 
-    @GetMapping(value = "/all")
-    public ResponseEntity<CommonResponse<List<ProvinceDto>>> getAllProvince() {
+    @GetMapping(value = "")
+    public ResponseEntity getAllProvince() {
+        List<ProvinceDto> allProvince = provinceService.getAllProvince();
+        return ResponseEntity.ok(new CommonResponse<List<ProvinceDto>>(true, allProvince));
+    }
+
+    @GetMapping(value = "/names")
+    public ResponseEntity getAllProvinceNames() {
         List<ProvinceDto> allProvince = provinceService.getAllProvince();
         return ResponseEntity.ok(new CommonResponse<List<ProvinceDto>>(true, allProvince));
     }
